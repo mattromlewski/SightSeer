f4a352f19a75502b07bd72c08950c9b600749b70
mattromlewski <romlewski.m@gmail.com>
2017-12-08 05:11:35 +0000

    initial compilation with opencv and libfreenect

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <libfreenect/libfreenect.hpp>
+
+
+using namespace std;
+
+int main()
+{
+    cv::Mat myMat;
+    cout << "Hello world!" << endl;
+    return 0;
+}
